0b252c55bab2a4e51faa9e6663c7bfa9f52c2b5a
ZephyrIt0 <it@zephyr-enr.fr>
2025-05-15 12:10:18 +0200

    Ajout du projet MPLAB

---
+++ MPLAB/parserJSON.X/build/default/debug/parserJSON.o.d
@@ -0,0 +1,5 @@
+build/default/debug/parserJSON.o: parserJSON.c parserJSON.h jsmn.h
+
+parserJSON.h:
+
+jsmn.h:
---
+++ MPLAB/parserJSON.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=parserJSON.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/parserJSON.X.production.hex
